89d37990843253d84e611570fae6a531d41d0b00
gggxbbb <gggxbbb@foxmail.com>
2026-07-10 22:35:11 +0800

    chore: initialize Vue 3 + Vite project scaffold

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <h1>学位绩点计算器</h1>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: false,
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/analysis\.gxb\.icu\/.*/,
+            handler: 'NetworkOnly'
+          }
+        ]
+      }
+    })
+  ]
+})
